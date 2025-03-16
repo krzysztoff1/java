39a83bcb561926aaf0b2f152754e88e1403ed47e
Krzysztof <halidu13@gmail.com>
2025-03-16 12:11:57 +0100

    Init

---
+++ src/main/java/com/example/internet2/Internet2Application.java
@@ -0,0 +1,13 @@
+package com.example.internet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Internet2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Internet2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/internet2/Internet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Internet2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
